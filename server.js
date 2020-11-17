8793c891686929906dd6186525ecf0d69fa14002
Marat <razorlevel@gmail.com>
2020-11-17 19:37:10 +0300

    wrap created ,fixed bugs with server ,added handlebars ,added styles for wrap

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const fs = require('fs')
 const path = require('path')
-const app = require('express')()
+const express = require('express')
+const app = express()
 const http = require('http').createServer(app)
 const PORT = process.env.PORT || 5000
+const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const viewsDir = path.join(__dirname, 'views')
+const hbs = exphbs.create({
+  defaultLayout: 'index',
+  extname: 'hbs',
+  layoutsDir: viewsDir + '/layouts',
+  partialsDir: [viewsDir + '/partials'],
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, './views/layouts'))
+
+app.use(express.static(path.join(__dirname, '/public')))
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header(
@@ -14,24 +29,16 @@ app.use(function (req, res, next) {
   next()
 })
 
-const htmlRender = (fileName) => {
-  fs.readFile(path.join(__dirname, 'public', fileName), (err, data) => {
-    if (err) throw err
-
-    res.end(data)
-  })
-}
-
 app.get('/favicon.ico', (req, res) => res.sendStatus(200))
 
 app.get('/', (req, res) => {
-  htmlRender('index.html',res)
+  res.render('index')
 })
 
-app.post('/', urlencodedParser, (req, res) => {
+app.post('/form', urlencodedParser, (req, res) => {
   if (!req.body) return res.sendStatus(400)
-  console.log(req.body)
-  htmlRender('sucsess.html',res)
+  console.log(JSON.parse(JSON.stringify(req.body)))
+  res.redirect('/')
 })
 
 http.listen(PORT, () => console.log(`Server started on port : ${PORT}`))
